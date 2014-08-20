4ca33b45368382cdfb82337a420606955f843966
tseng611 <gizu.tseng@gmail.com>
2014-08-20 10:15:26 -0400

    my first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## cachematrix.R - Programming Assignment 2: Lexical Scoping
+## Below are two functions that are used to create a special matrix object 
+## that stores a numeric matrix and cache's its inverse.
+## (Example used: Caching the Mean of a Vector)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(mtx) inv <<- mtx
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
-
-
-## Write a short comment describing this function
-
+## This function calculates the inverse of the special "matrix" object 
+## created with the above function. 
+## 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setinv(m)
+        m
 }
+
+## How to test:
+## source ("cachematrix.R")
+## c <- rbind(c(1, -1/4), c(-1/4, 1))
+## class(c)
+## dim (c)
+## cc <- makeCacheMatrix(c)
+## 
+## cacheSolve(cc)
+## 
+## getting cached data
+## cacheSolve(cc)
+##
